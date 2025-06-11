5c5cf31a263cee02a131eb597768eff0aad911c2
numerounodeveloper <numerounodeveloper@gmail.com>
2025-06-11 19:58:25 +0530

    style fix

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -5,9 +5,11 @@ import './BookCard.css';
 const BookCard = ({ book, id }) => {
   return (
     <div className="book-card">
+      <div className='bookInfo'>
       <img src={book.imageLinks?.thumbnail || ''} alt={book.title} />
       <h3>{book.title}</h3>
       <p className="authors">{book.authors?.join(', ')}</p>
+      </div>
       <Link to={`/book/${id}`} className="details-button">View Details</Link>
 
 
